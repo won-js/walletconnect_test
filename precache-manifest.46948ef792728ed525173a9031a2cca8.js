3e4a6d9dee846bdcf62a441fb12bc040f91ed8f1
wonjs <wonjs@sk.com>
2022-11-09 10:38:09 +0900

    Updates

---
+++ precache-manifest.46948ef792728ed525173a9031a2cca8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cf87bf8159d1a185b7f",
+    "url": "/walletconnect_test/static/js/main.7cf87bf8.chunk.js"
+  },
+  {
+    "revision": "72564c0c4aa468fbe1f3",
+    "url": "/walletconnect_test/static/js/1.72564c0c.chunk.js"
+  },
+  {
+    "revision": "a824b1c6ad4c2422b4a8",
+    "url": "/walletconnect_test/static/js/runtime~main.a824b1c6.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/walletconnect_test/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "906078994f7221a25b900f47425b3ecc",
+    "url": "/walletconnect_test/static/media/erc20.90607899.svg"
+  },
+  {
+    "revision": "42bca7317690c3f5e9ecd8db387344df",
+    "url": "/walletconnect_test/static/media/eth.42bca731.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/walletconnect_test/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "462c290106395e82d026edc024006a03",
+    "url": "/walletconnect_test/index.html"
+  }
+];
